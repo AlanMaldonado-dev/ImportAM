744d2b201287f7561d5f33860a76d786cb7a6d8c
Alan Maldonado <gabrielalanmaldonado@gmail.com>
2025-09-30 11:51:46 -0300

    add image

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,8 +1,15 @@
 const HeroSection = () => {
     return (
-        <div>
-           <img className=" h-[600px] w-full" src="../../../public/hero-img.jpg" alt="Imagen de muestra"/>
-        </div>
+        // <div>
+        //    <img className=" h-[600px] w-full" src="../../../public/hero-img.jpg" alt="Imagen de muestra"/>
+        // </div>
+        <div className="bg-black flex items-center justify-center"> {/* ocupa toda la pantalla */}
+      <img
+        src="https://i.ibb.co/9kRv40DF/hero-img.png"
+        alt="Hero background"
+        className="object-cover h-[300px] w-[300px]"
+      />
+    </div>
     );
 }
 
